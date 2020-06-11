244119066c4fa40b7b54279e750f92b620ff7181
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-11 23:06:13 +0100

    Changed App syntax to arrow function instead

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Navigation/>
