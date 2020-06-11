a9addec0eccf4d7754b976359d25496a08a80737
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-11 23:05:09 +0100

    Imported logo image and add content and styles to Logo component

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Tilt from "react-tilt";
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
   return (
-    <h1>Hello World</h1>
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{max: 55}} style={{height: 150, width: 150}}>
+        <div className="Tilt-inner pa3"><img style={{paddingTop: 5}} src={brain} alt="Logo"/></div>
+      </Tilt>
+    </div>
   )
 }
 
