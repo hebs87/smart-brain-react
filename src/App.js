69c110495b1c363e28bb7ef3cec066273706c0ab
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-11 23:34:44 +0100

    Added Particles to App for background effects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import './App.css';
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 125,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 const App = () => {
   return (
     <div className="App">
+      <Particles
+        className='particles'
+        params={particleOptions}
+      />
       <Navigation/>
       <Logo/>
       <Rank/>
